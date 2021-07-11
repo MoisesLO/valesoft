d9a5f714a2a00d7a999c8092e892040c72690f0c
moises <elnaufrago2009@gmail.com>
2021-07-11 17:20:32 -0500

    edit

--- views/layout/web/header.php
+++ views/layout/web/header.php
@@ -6,7 +6,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Simple example using Multi Theme</title>
-	<link rel="stylesheet" href="/assets/css/style.css">
+  <!-- Tailwind Css -->
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.3.5/dist/alpine.min.js" defer></script>
 </head>
 <body class="dark:bg-gray-900">
